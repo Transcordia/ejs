48a3c74a2c53bded974cae2865c06ddbd6f5d3fa
jcook <jcook@pykl.com>
2014-02-13 15:33:49 -0500

    Using the new ejs.io repository. Added parameters example.

--- src/main/webapp/WEB-INF/app/main.js
+++ src/main/webapp/WEB-INF/app/main.js
@@ -1,11 +1,16 @@
 var {Application} = require( 'stick' );
-var response = require('ringo/jsgi/response');
+var response = require( 'ringo/jsgi/response' );
 
 var app = exports.app = new Application();
-app.configure( 'route' );
+app.configure( 'params', 'route' );
 
-app.get( '/', function ( req, name ) {
-    return response.html('Hello, World');
+app.get( '/', function ( req ) {
+    var name = req.params.name || 'World';
+    return response.html( 'Hello, ' + name );
+} );
+
+app.get( '/:name', function ( req, name ) {
+    return response.html( 'Hello, ' + name );
 } );
 
 
